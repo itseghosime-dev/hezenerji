2f9700f6603b66bc253279d804ae4ce753f10579
itseghosime-dev <info.itseghosime@gmail.com>
2026-09-15 20:24:33 +0100

    fix: added a commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
+      {/* Learning Github */}
       <ExpertiseSection />
       <CompanyVideoSection />
       <FlagshipProject />
